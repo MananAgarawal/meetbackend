7d07d3ced236b3dc8ba0ea77794df5ef75eb0e0d
Manan Agarwal <maverickmanan@gmail.com>
2023-11-22 17:22:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+const cors = require("cors");
+app.use(cors());
+app.use(jsonParser);
+require("dotenv").config();
+
+const logsignroute = require("./Routes/users")
+const sumissionrouter = require("./Routes/submits")
+const problemrouter = require("./Routes/problems")
+
+// connection
+const {connectmongodb} = require('./connection')
+
+connectmongodb(`mongodb+srv://${process.env.USER_API_USER}:${process.env.USER_API_KEY}@cluster.tfhewau.mongodb.net/?retryWrites=true&w=majority`);
+
+// routes
+app.use("/submission", sumissionrouter);
+app.use("/",logsignroute);
+app.use("/problems", problemrouter)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
